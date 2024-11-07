930f2cb8079b8253b3013a0a1cc3d1f0e654e1b4
MasterVVK <vvk@2140101.ru>
2024-11-07 23:41:51 +0500

    Update test.py

--- test.py
+++ test.py
@@ -15,8 +15,9 @@ img_desc = post_gen.generate_post_image_description()
 img_gen = ImageGenerator(conf.openai_key)
 image_url = img_gen.generate_image(img_desc)
 
+print(content)
+print(image_url)
+
 vk_pub = VKPublisher(conf.vk_api_key, conf.vk_group_id)
 vk_pub.publish_post(content, image_url)
 
-print(content)
-print(image_url)
