d97bc0d58d7d52a46655cf9cd6ee96689863b415
MasterVVK <vvk@2140101.ru>
2024-11-07 22:36:18 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 from generators.text_gen import PostGenerator
-from generators.image_gen import ImageGenerator 
+from generators.image_gen import ImageGenerator
+from social_publishers.vk_publisher import VKPublisher
 import config as conf
+import os
 
-post_gen = PostGenerator(conf.openai_key, tone="позитивный и весёлый", topic="Новая коллекция кухонных ножей от компании ZeroKnifes")
+# Установка прокси перед инициализацией классов
+os.environ["http_proxy"] = conf.http_proxy
+os.environ["https_proxy"] = conf.https_proxy
+
+post_gen = PostGenerator(conf.openai_key, tone="солидный и доверительный", topic="Новая коллекция ювелирных украшений от компании ЗлатоМаркет")
 content = post_gen.generate_post()
 img_desc = post_gen.generate_post_image_description()
 
 img_gen = ImageGenerator(conf.openai_key)
 image_url = img_gen.generate_image(img_desc)
+
+vk_pub = VKPublisher(conf.vk_api_key, conf.vk_group_id)
+vk_pub.publish_post(content, image_url)
 
 print(content)
 print(image_url)
